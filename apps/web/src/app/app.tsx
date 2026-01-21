bd4e7dc08997ee7db408fb6fbf7d5d5774a727b1
neb42 <ben.mcalindin@nrc.no>
2026-01-21 15:49:44 +0100

    feat(02-03): show board on game start

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,12 +1,25 @@
 import Lobby from '../components/Lobby';
+import { HexGrid } from '../game/Board/HexGrid';
+import { useGameStore } from '../stores/gameStore';
 
 export function App() {
+  const gameState = useGameStore((state) => state.gameState);
+
   return (
     <>
-      <div className="bg-pattern" />
-      <div className="hex-decoration hex-1" />
-      <div className="hex-decoration hex-2" />
-      <Lobby />
+      {!gameState && (
+        <>
+          <div className="bg-pattern" />
+          <div className="hex-decoration hex-1" />
+          <div className="hex-decoration hex-2" />
+        </>
+      )}
+
+      <div style={{ display: gameState ? 'none' : 'block' }}>
+        <Lobby />
+      </div>
+
+      {gameState && <HexGrid />}
     </>
   );
 }
